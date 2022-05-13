202b641794b736d65854802c8c827df2cdddc221
mnmnyp8pa1234567 <105113886+mnmnyp8pa1234567@users.noreply.github.com>
2022-05-13 12:33:55 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -6,11 +6,11 @@ class Var((object)):
     # 6 is a placeholder
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
-    DB_URI = os.environ.get("DATABASE_URL", None)
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", None)
+    DB_URI = os.environ.get("DATABASE_URL", False)
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", False)
     LOGGER = True
-    GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
-    GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
+    GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", False)
+    GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", False)
     # Here for later purposes
     SUDO_USERS = {int(x) for x in os.environ.get("SUDO_USERS", "1418571871").split()}
 
@@ -21,24 +21,24 @@ class Var((object)):
     DEVLOPERS = {int(x) for x in os.environ.get("DEVLOPERS", "").split()}
     OWNER_ID = {int(x) for x in os.environ.get("OWNER_ID", "").split()}
     SUPPORT_USERS = {int(x) for x in os.environ.get("SUPPORT_USERS", "").split()}
-    PLUGIN_CHANNEL = int(os.environ.get("REBELBOT_ID", None))
-    LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-    TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
-    TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", None)
-    DOWNLOAD_PFP_URL_CLOCK = os.environ.get("DOWNLOAD_PFP_URL_CLOCK", None)
-    PM_PERMIT_GROUP_ID = os.environ.get("PM_PERMIT_GROUP_ID", None)
-    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
-    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
+    PLUGIN_CHANNEL = int(os.environ.get("REBELBOT_ID", False))
+    LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", False)
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", False)
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", False)
+    TG_BOT_TOKEN_BF_HER = os.environ.get("TG_BOT_TOKEN_BF_HER", False)
+    TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", False)
+    DOWNLOAD_PFP_URL_CLOCK = os.environ.get("DOWNLOAD_PFP_URL_CLOCK", False)
+    PM_PERMIT_GROUP_ID = os.environ.get("PM_PERMIT_GROUP_ID", False)
+    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", False)
+    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", False)
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
-    AUTH_TOKEN_DATA = os.environ.get("AUTH_TOKEN_DATA", None)
-    if AUTH_TOKEN_DATA != None:
+    AUTH_TOKEN_DATA = os.environ.get("AUTH_TOKEN_DATA", False)
+    if AUTH_TOKEN_DATA != False:
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
         with open(TEMP_DOWNLOAD_DIRECTORY + "auth_token.txt", "w") as t_file:
             t_file.write(AUTH_TOKEN_DATA)
-    REBELBOT_ID = os.environ.get("REBELBOT_ID", None)
-    if REBELBOT_ID != None:
+    REBELBOT_ID = os.environ.get("REBELBOT_ID", False)
+    if REBELBOT_ID != False:
         try:
             REBELBOT_ID = int(REBELBOT_ID)
         except ValueError:
