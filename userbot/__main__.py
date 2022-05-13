fa34fc39c82f390f2ec7cb0c773a62dde28849ef
mnmnyp8pa1234567 <105113886+mnmnyp8pa1234567@users.noreply.github.com>
2022-05-13 12:55:15 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -5,12 +5,12 @@ from sys import argv
 import telethon.utils
 from telethon import TelegramClient
 
-from userbot import REBELversion, bot, bot1, bot2, bot3, bot4, LOGS
+from userbot import REBELversion, bot, bot1, bot2, bot3, bot4
 from userbot.utils import load_module
 from var import Var
-from userbot.Config import Config
 
 os.system("pip install -U telethon")
+
 
 async def add_bot(bot_token):
     await bot.start(bot_token)
@@ -26,6 +26,10 @@ if len(argv) not in (1, 3, 4):
     bot4.disconnect()
 else:
     bot.tgbot = None
+    bot1.tgbot = None
+    bot2.tgbot = None
+    bot3.tgbot = None
+    bot4.tgbot = None
     if Var.TG_BOT_USER_NAME_BF_HER is not None:
         print("Initiating Inline Bot")
         # ForTheGreatrerGood of beautification
@@ -35,7 +39,7 @@ else:
         print("Initialisation finished with no errors")
         print("🔰ՏTᗩᖇT ᗪᗩᖇK ᗯᗴᗷ🔰")
         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
-        print("⚡ᗪᗩᖇK ᗯᗴᗷ ՏᗴTᑌᑭ ᑕOᗰᑭᒪᗴTᗴᗪ⚡")
+        print("⚡ᗪᗩᖇK ᗯᗴᗷ ՏTᗩᖇTᑌᑭ ᑕOᗰᑭᒪᗴTᗴᗪ⚡")
     else:
         bot.start()
         bot1.start()
@@ -54,9 +58,10 @@ for name in files:
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
 
-
-print(f"""Hello sir i am DARK WEB!! DARK WEB VERSION :- {REBELversion} YOUR REBELBOT IS READY! FOR CHECK YOUR BOT WORKING OR NOT PLEASE TYPE (.alive/.ping) ENJOY YOUR BOT! JOIN FOR MORE FUTURE UPDATES @DARK_WEB_ARMY ."""
+print(
+    f"""Hello sir i am !!  VERSION :- 50 YOUR  IS READY! FOR CHECK YOUR BOT WORKING OR NOT PLEASE TYPE (.alive/.ping) ENJOY YOUR BOT! JOIN FOR MORE FUTURE UPDATES @DARK_WEB_ARMY ."""
 )
+
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
